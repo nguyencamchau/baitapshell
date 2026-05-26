2b04bf0fd6f4c6c54a5748b9069ab8e165e8604b
nguyencamchau <nguyencamchau455@gmail.com>
2026-05-26 11:04:07 +0700

    Chuong trinh tinh toan

--- tinhdt.sh
+++ tinhdt.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#Shebang (hoặc Hashbang, Shabang)
+#!/bin/bash 
 
 # Hiển thị thông báo và nhận dữ liệu nhập vào
 read -p "Nhập bán kính hình tròn (r): " radius
@@ -11,3 +12,13 @@ dien_tich=$(awk "BEGIN {print $pi * $radius * $radius}")
 
 # In kết quả ra màn hình với 2 chữ số thập phân (định dạng %.2f)
 printf "Diện tích hình tròn có bán kính %s là: %.2f\n" "$radius" "$dien_tich"
+
+# Hiển thị thông báo và nhận số từ bàn phím
+read -p "Nhập vào một số nguyên: " number
+
+# Kiểm tra số chẵn lẻ bằng toán tử % (chia lấy dư)
+if [ $((number % 2)) -eq 0 ]; then
+    echo "Số $number là số chẵn."
+else
+    echo "Số $number là số lẻ."
+fi
